8baba63e9ddd2bedca6e06311069e0677805ec6f
Shafy Hafidz <72294926+sh4fyhafidz@users.noreply.github.com>
2025-03-06 13:26:57 +0700

    Update 4 Data Overview.py

--- FINPRO_GAMMA_JCDS0508_Strimlit/pages/4 Data Overview.py
+++ FINPRO_GAMMA_JCDS0508_Strimlit/pages/4 Data Overview.py
@@ -1,7 +1,5 @@
-import pandas as pd
 import streamlit as st
-import requests
-import io
+import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="Used Cars SA Data Overview",
@@ -9,16 +7,11 @@ st.set_page_config(
     layout="wide")
 
 st.title("📊 Data Overview")
-st.write('With this tool, you can see an overview of the dataset and the average of each column.')
-st.markdown('---')
-
-# Embed Tableau Dashboard
-st.markdown("## 📈 Interactive Dashboard")
 st.write("Below is an interactive dashboard for further insights:")
-st.markdown(
-    """
-    <iframe src="https://public.tableau.com/views/UsedCarDashboard_17410944222040/Dashboard2?:showVizHome=no&:embed=true"
-    width="100%" height="600" style="border: none;"></iframe>
-    """,
-    unsafe_allow_html=True
+
+tableau_url = "https://public.tableau.com/views/UsedCarDashboard_17410944222040/Dashboard2?:showVizHome=no&:embed=true"
+
+components.html(
+    f'<iframe src="{tableau_url}" width="100%" height="600"></iframe>',
+    height=600
 )
